8278ec543b44d594f2c9c21c6a6dfc99db5031e9
Adrian Hansen <adrian.hansen.flensburg@gmail.com>
2026-05-25 16:11:00 +0200

    add room repository

---
+++ backend/src/main/java/com/tokiohotel/backend/repository/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.tokiohotel.backend.repository;
+
+import com.tokiohotel.backend.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Integer> {
+}
